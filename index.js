fe31b2aba34bb1537d19e553c9dd0cf8a8daeee5
Ilia <sorockiniliuh@yandex.ru>
2022-09-05 20:57:46 +0300

    rel-0,rel-1

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-// Release 0
+let student = {
+firstname : 'petr',
+lastname : 'Perviy'
+}  
+let firstname = 'Petya'
+delete student.firstname
+     // Release 0
 
-// Release 1
+
+let group = [] 
+group = [{firstname : 'petr',lastname : 'Perviy'}] ;
+ group = [{firstname : 'petr',lastname : 'Perviy'},{firstname : 'Darth',lastname : 'Vaider'},{firstname : 'Jack',lastname : 'Sparrow'}]  
+ 
+ console.log(group)// Release 1
